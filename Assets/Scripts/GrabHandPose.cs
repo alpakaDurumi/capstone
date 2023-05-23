4e81bad81cbc670b2bc98a6a1776895c170a0607
alpakaDurumi <durams@naver.com>
2023-05-23 11:44:22 +0900

    Fix : Adjust Rotation when Grab Sword
    - Fix GrabHandPose.cs to adjust hand position and rotation with
    Attach Point, and Finger Bones with script.
    - Add Sword Prefab : Assets/Prefabs/Weapons/

--- Assets/Scripts/GrabHandPose.cs
+++ Assets/Scripts/GrabHandPose.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-// 무기를 그랩했을 때 포즈를 지정하기 위한 스크립트
+// 무기를 그랩했을 때 손가락의 포즈를 지정하기 위한 스크립트
 public class GrabHandPose : MonoBehaviour
 {
     public HandData rightHandPose;
-
-    private Vector3 startingHandPosition;
-    private Vector3 finalHandPosition;
-    private Quaternion startingHandRotation;
-    private Quaternion finalHandRotation;
 
     private Quaternion[] startingFingerRotations;
     private Quaternion[] finalFingerRotations;
@@ -24,44 +19,40 @@ public class GrabHandPose : MonoBehaviour
         grabInteractable.selectEntered.AddListener(SetupPose);
         grabInteractable.selectExited.AddListener(UnSetPose);
 
+        // Grab 시의 손 모양을 나타내는 오브젝트는 보이지 않게 해둠
         rightHandPose.gameObject.SetActive(false);
     }
 
     public void SetupPose(BaseInteractionEventArgs arg) {
         if (arg.interactorObject is XRDirectInteractor) {
             // 무기를 select한 DirectInteractor, 즉 오른손의 HandData를 참조한다
+            // 런타임에 prefab으로 생성되는 손 모델은 DirectInteractor와 형제 관계
             HandData handData = arg.interactorObject.transform.parent.GetComponentInChildren<HandData>();
             // select했을 때 애니메이터도 비활성화 
             handData.animator.enabled = false;
 
             // 현재 오른손의 데이터와 무기를 잡은 손모양의 HandData를 각각 받아온다.
-            SetHandDataValues(handData, rightHandPose);
+            GetFingerData(handData, rightHandPose);
             // 값 설정하기
-            SetHandData(handData, finalHandPosition, finalHandRotation, finalFingerRotations);
+            SetFingerData(handData, finalFingerRotations);
         }
     }
 
     public void UnSetPose(BaseInteractionEventArgs arg) {
         if (arg.interactorObject is XRDirectInteractor) {
-            // 무기를 select한 DirectInteractor, 즉 오른손의 HandData를 참조한다
+            // 무기를 unselect한 DirectInteractor, 즉 오른손의 HandData를 참조한다
             HandData handData = arg.interactorObject.transform.parent.GetComponentInChildren<HandData>();
-            // select했을 때 애니메이터도 비활성화 
+            // 비활성화 했던 애니메이터를 다시 활성화
             handData.animator.enabled = true;
 
             // 값 설정하기
-            SetHandData(handData, startingHandPosition, startingHandRotation, startingFingerRotations);
+            SetFingerData(handData, startingFingerRotations);
         }
     }
 
 
-    // 시작과 끝으로 삼을 두 HandData를 설정하는 함수
-    public void SetHandDataValues(HandData h1, HandData h2) {
-        startingHandPosition = h1.root.localPosition;
-        finalHandPosition = h2.root.localPosition;
-
-        startingHandRotation = h1.root.localRotation;
-        finalHandRotation = h2.root.localRotation;
-
+    // 시작과 끝으로 삼을 두 쌍의 손가락 본 회전 값들을 얻어오는 함수
+    public void GetFingerData(HandData h1, HandData h2) {
         startingFingerRotations = new Quaternion[h1.fingerBones.Length];
         finalFingerRotations = new Quaternion[h2.fingerBones.Length];
 
@@ -71,11 +62,8 @@ public class GrabHandPose : MonoBehaviour
         }
     }
 
-    // 새로운 값들을 HandData에 할당하는 함수
-    public void SetHandData(HandData h, Vector3 newPosition, Quaternion newRotation, Quaternion[] newBonesRotation) {
-        h.root.localPosition = newPosition;
-        h.root.localRotation = newRotation;
-
+    // 새로운 손가락 본 회전 값들을 HandData에 할당하는 함수
+    public void SetFingerData(HandData h, Quaternion[] newBonesRotation) {
         for (int i = 0; i < newBonesRotation.Length; i++) {
             h.fingerBones[i].localRotation = newBonesRotation[i];
         }
--- Assets/Scripts/HandData.cs
+++ Assets/Scripts/HandData.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 손 모델의 각 손가락 본들의 값을 가져오기 위한 스크립트
 public class HandData : MonoBehaviour
 {
     public enum HandModelType { Left, Right };
 
     public HandModelType handType;
-    public Transform root;
     public Animator animator;
     public Transform[] fingerBones;
 
