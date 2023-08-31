04c7e98dab06faab9663a4d21b2e44ee42d0e86f
howudong <tjdvy963@naver.com>
2023-08-31 15:11:39 +0900

    feat : GameManager 스크립트 생성 및 싱글톤화 & StageMove 함수 추가

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneTransition : MonoSingleton<SceneTransition>
+public class SceneTransition : MonoBehaviour
 {
     private FadeScreen fadeScreen;
 
