0d4dc83b17b4f399b45b88aba6b415945b970fe0
howudong <tjdvy963@naver.com>
2023-09-20 23:52:27 +0900

    feat : 게임의 진행을 담당하는 게임 매니저 싱글톤 생성

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoSingleton<GameManager>
 {
-    [SerializeField] SceneTransition sceneTransition;
+
     [SerializeField] int stage;
+    private int remainEnemy;
 
     private void Awake()
     {
-        sceneTransition = gameObject.AddComponent<SceneTransition>();
-        stage = 0;
+        stage = 1;
+        
+    }
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
     }
 
-    public void MoveTargetStage(int target)
+    public void DecreaseRemainEnemy()
     {
-        sceneTransition.GoToSceneAsync(target);
+        Debug.Log(remainEnemy--);
+        // 모든 적을 해치우면 다음 스테이지로 이동
+        if(remainEnemy <= 0)
+        {
+            SceneController.Instance.GoToScene(++stage);
+        }
     }
+    private void SceneManager_sceneLoaded(Scene scene, LoadSceneMode load)
+    {
+        remainEnemy = ScanAllEnemy();
+    }
+
+
+    private int ScanAllEnemy()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Debug.Log(enemies.Length);
+        return enemies.Length;
+    }
+    
 }
