57b2edc1d47a67a0c4a18376266335de8b5b1d0f
alpakaDurumi <durams@naver.com>
2023-10-20 14:30:10 +0900

    Fix : misc
    - fix duplicate collision when enemy hit by Axe
        - Axe.cs
    - change layer of direct interactor to "Ignore Raycast"
        - XR Origin.prefab
    - Chore interface
        - IProjectileHittable.cs

--- Assets/Scripts/Axe.cs
+++ Assets/Scripts/Axe.cs
@@ -7,23 +7,19 @@ public class Axe : Projectile
 {
     private IXRSelectInteractor thrownInteractor;   // 도끼를 던진 손에 해당하는 Direct Interactor
 
-    protected override void OnSelectEntered(SelectEnterEventArgs args) {
-        base.OnSelectEntered(args);
-        launched = false;
-    }
-
     protected override void OnSelectExited(SelectExitEventArgs args) {
         base.OnSelectExited(args);
         EnablePhysics();    // 손을 떠나면 다시 물리 활성화
         launched = true;
-        thrownInteractor = args.interactorObject;
+        thrownInteractor = args.interactorObject;   // 던진 interactor를 기억
     }
 
     private void OnTriggerEnter(Collider other) {
         // 투척된 상태에서 충돌했다면 충돌 오브젝트가 ProjectileTarget 컴포넌트를 가지는지 검사
         if (launched && other.gameObject.layer != LayerMask.NameToLayer("Ignore Raycast")) {
+            launched = false;   // 중복 충돌을 방지하기 위함
             DisablePhysics();   // 물리 비활성화(꽂혀있는 효과)
-
+            
             ProjectileTarget target = other.transform.GetComponentInParent<ProjectileTarget>();
             // 가지고 있다면
             if(target != null) {
@@ -37,7 +33,7 @@ public class Axe : Projectile
         }
     }
 
-    // 1.0초 후 던진 손으로 다시 select 되도록 하는 코루틴
+    // 1.0초 후 던진 interactor로 다시 select 되도록 하는 코루틴
     private IEnumerator ReturnRoutine() {
         yield return new WaitForSeconds(1.0f);
         interactionManager.SelectEnter(thrownInteractor, this);
--- Assets/Scripts/IProjectileHittable.cs
+++ Assets/Scripts/IProjectileHittable.cs
@@ -3,4 +3,5 @@ using UnityEngine;
 public interface IProjectileHittable
 {
     void Hit(RaycastHit hit, Projectile projectile);
+    void Hit(Transform hitTransform, Projectile projectile);
 }
