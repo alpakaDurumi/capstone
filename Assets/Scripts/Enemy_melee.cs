de0125f61aba75902e2e381a0df6c0be74c120dc
alpakaDurumi <durams@naver.com>
2023-10-30 18:24:20 +0900

    Refactor : Enemy scripts
    - Refactor scripts
        - Enemy.cs
        - Enemy_melee.cs
        - Enemy_rifle.cs
        - ProjectileTarget.cs
    - Adjust Exit Time of animation transition
        - Animator Controller-Melee.controller
        - Animator Controller-Rifle.controller
    - Remove Animation Events
        - standing melee attack 360 high.anim
        - standing melee attack backhand.anim
        - standing melee attack downward.anim
        - standing melee attack horizontal.anim
        - standing melee run jump attack.anim

--- Assets/Scripts/Enemy_melee.cs
+++ Assets/Scripts/Enemy_melee.cs
@@ -5,30 +5,8 @@ using UnityEngine.AI;
 
 public class Enemy_melee : Enemy
 {
-    private bool attacking = false;
-
-    protected override void Update()
-    {
-        base.Update();
-
-        if(!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance && !attacking) {
-            attacking = true;
-            agent.isStopped = true;
-            Attack();
-        }
-
+    protected override void Attack() {
+        base.Attack();
+        animator.SetInteger("attack index", Random.Range(0, 4));    // 4개의 공격 모션 중 랜덤 하나
     }
-
-    // 공격 함수
-    private void Attack() {
-        animator.SetInteger("attack index", Random.Range(0, 4));
-        animator.SetTrigger("attack");
-    }
-
-    // 공격 애니메이션이 끝났을 때 호출되는 이벤트 함수
-    private void AttackEnd() {
-        attacking = false;
-        agent.isStopped = false;
-    }
-
 }
--- Assets/Scripts/Enemy_rifle.cs
+++ Assets/Scripts/Enemy_rifle.cs
@@ -5,67 +5,44 @@ using UnityEngine.AI;
 
 public class Enemy_rifle : Enemy
 {
-    private bool attacking = false;
     private bool reloading = false;
 
-    float attack_timer = 0.0f;
-    float attack_waitingTime = 2.0f;
-
-    int attack_cnt = 0;
-    int magazine = 4;
+    private int attack_cnt = 0;
+    private int magazine = 4;
 
     protected override void Update()
     {
         base.Update();
 
-        // 공격이 끝나있고 장전 중이 아닐 때에만 타이머를 증가
-        if (!attacking && !reloading) {
-            attack_timer += Time.deltaTime;
-        }
-
         // 총알 모두 소모 시 재장전
         if(attack_cnt == magazine) {
             Reload();
+            StartCoroutine(WaitReloadEnd((b) => { reloading = b; }));
         }
-
-        if(!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance && !attacking) {
-            // attack_waitingTime에 한 번씩 공격하도록 하였음
-            if(attack_timer >= attack_waitingTime) {
-                attacking = true;
-                Attack();
-                attack_timer = 0.0f;
-            }
-            agent.isStopped = true;
-        }
-        // 사정거리에 들어왔지만 공격은 못한 상태에서 다시 target이 멀어졌을 때 다시 움직이게 하기 위해
-        else {
-            agent.isStopped = false;
-        }
-
     }
 
-    // 공격 함수
-    private void Attack() {
-        animator.SetTrigger("attack");
+    // 공격 가능 여부
+    protected override bool CanAttack() {
+        if (agent.isStopped && !attacking && attack_timer >= attack_waitingTime && !reloading) {
+            return true;
+        }
+        return false;
+    }
+
+    protected override void Attack() {
+        base.Attack();
         attack_cnt++;
-        Debug.Log(attack_cnt);
-    }
-
-    // 공격 애니메이션이 끝났을 때 호출되는 이벤트 함수
-    private void AttackEnd() {
-        attacking = false;
-        agent.isStopped = false;
     }
 
     // 재장전 함수
     private void Reload() {
+        reloading = true;
         animator.SetTrigger("reload");
         attack_cnt = 0;
     }
 
-    // 재장전 애니메이션이 끝났을 때 호출되는 이벤트 함수
-    private void ReloadEnd() {
-        reloading = false;
+    private IEnumerator WaitReloadEnd(System.Action<bool> callback) {
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("Reload") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f);
+        callback(false);
     }
-
 }
